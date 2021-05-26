5355bfee7c54801d3caad1b394105bdd84447152
bpartida <bpartida@clarku.edu>
2021-05-26 00:22:13 -0400

    Server adding and updating functions

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 //Initialize passport
@@ -23,7 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/danceLightDB',
-    {useNewUrlParser: true}, function () {
+    { useNewUrlParser: true }, function () {
         console.log("db connection successful");
     });
 
@@ -34,7 +34,8 @@ const danceSchema = {
     cues: [{
         start_time: Number,
         end_time: Number,
-        lights: [{light_name: String, color: String, brightness: String}]
+        sort_order: Number,
+        lights: [{ light_name: String, color: String, brightness: String }]
     }]
 }
 
@@ -46,20 +47,20 @@ const showSchema = {
         type: String,
         required: true
     },
-    show_name:{
+    show_name: {
         type: String,
         require: [true, "Show must be named"]
     },
-    contact_name:{
-        type:String,
+    contact_name: {
+        type: String,
         require: [true, "Contact Name cannot be empty"]
     },
-    contact_email:{
-        type:String,
+    contact_email: {
+        type: String,
         require: [true, "Contact String cannot be empty"]
     },
-    contact_phone:{
-        type:String,
+    contact_phone: {
+        type: String,
         require: [true, "Contact  cannot be empty"]
     },
     show_dates: [{
@@ -81,7 +82,7 @@ const showSchema = {
         }
     }],
     dances: [
-        {dance_id: Number }
+        { dance_id: String, sort_order: Number }
     ]
 }
 
@@ -126,57 +127,28 @@ app.listen(3001, function () {
     console.log("server started at 3001");
 });
 
-// Register a User
-// app.get('/node_register', (req, res) => {
-//     if (req.query.error) {
-//         // ERROR
-//         // res.redirect("/register.html?error=" + req.query.error);
-//     } else {
-//         // ERROR
-//         // res.redirect("/register.html");
-//     }
-// });
+// Register New User
+app.post('/node_register', (req, res) => {
 
-// Login a User
-// app.get('/node_login', (req, res) => {
-//     if (req.query.error) {
-//         // ERROR
-//         // res.redirect("/login.html?error=" + req.query.error);
-//     } else {
-//         // ERROR
-//         // res.redirect("/login.html");
-//     }
-// });
-
-// Logout a User
-app.get('/node_logout', (req, res) => {
-    req.logout();
-    res.send({
-        message: "success"
-    })
-});
-
-app.post('/node_register', (req, res) =>{
-    //console.log('profile image is '+ img);
     const newUser = {
         username: req.body.username,
-        password: req.body.password,
         email: req.body.email,
         team_type: req.body.team_type,
     }
-    //console.log(req.body.password);
+
     User.register(
         newUser,
+
         req.body.password,
-        function (err, user){
+
+        function (err, user) {
             if (err) {
                 res.send({
                     message: "error",
                     data: err
                 });
-            }else {
+            } else {
                 console.log('success!!')
-                // write into cookies, authenticate the requests
                 const authenticate = passport.authenticate('local');
                 authenticate(req, res, function () {
                     res.send({
@@ -196,18 +168,18 @@ app.post('/node_login', (req, res) => {
         password: req.body.password,
 
     });
-    req.login(
-        user,
-        function (err) {
-            if (err){
-                res.send({
-                    message: "error",
-                    data: err
-                });
-            }else{
-                const authenticate = passport.authenticate(
-                    'local',
-                    {},
+
+
+    req.login(user, function (err) {
+        if (err) {
+            res.send({
+                message: "error",
+                data: err
+            });
+        } else {
+            const authenticate = passport.authenticate(
+                'local',
+                {},
                 (error, userExist, info) => {
                     if (userExist) {
                         res.send({
@@ -221,21 +193,28 @@ app.post('/node_login', (req, res) => {
                         });
                     }
                 });
-                authenticate(req,res);
-            }
+            authenticate(req, res);
         }
+    }
     )
 });
 
-// Current User
-app.get('/node_get_current_user', function (req,res){
-    if (req.isAuthenticated()){
-        //console.log(req.user);
+// Logout a User
+app.get('/node_logout', (req, res) => {
+    req.logout();
+    res.send({
+        message: "success"
+    })
+});
+
+// Get Current User
+app.get('/node_get_current_user', function (req, res) {
+    if (req.isAuthenticated()) {
         res.send({
             message: "success",
             data: req.user
         });
-    }else{
+    } else {
         res.send({
             message: "no login",
             data: {}
@@ -243,12 +222,34 @@ app.get('/node_get_current_user', function (req,res){
     }
 });
 
+// Get Show by Id
 app.get('/node_get_show_by_id', function (req, res) {
-    Show.findOne({"_id": req.query.show_id}, function (err, data) {
-        if (err || data.length === 0) {
+    Show.findOne(
+
+        { "_id": req.query.show_id }
+
+        , function (err, data) {
+            if (err || data.length === 0) {
+                res.send({
+                    "message": "internal database error",
+                    "data": {}
+                });
+            } else {
+                res.send({
+                    "message": "success",
+                    "data": data
+                })
+            }
+        });
+});
+
+// Get All Shows in database
+app.get("/node_get_all_shows", function (req, res) {
+    Show.find(function (err, data) {
+        if (err) {
             res.send({
                 "message": "internal database error",
-                "data": {}
+                "data": []
             });
         } else {
             res.send({
@@ -259,18 +260,162 @@ app.get('/node_get_show_by_id', function (req, res) {
     });
 });
 
+// Get Dance by Id
 app.get('/node_get_dance_by_id', function (req, res) {
-    Dance.findOne({"_id": req.query.dance_id}, function (err, data) {
-        if (err || data.length === 0) {
-            res.send({
-                "message": "internal database error",
-                "data": {}
+    Dance.findOne(
+
+        { "_id": req.query.dance_id }
+
+        , function (err, data) {
+            if (err || data.length === 0) {
+                res.send({
+                    "message": "internal database error",
+                    "data": {}
+                });
+            } else {
+                res.send({
+                    "message": "success",
+                    "data": data
+                })
+            }
+        })
+});
+
+// Get All Dances in a show
+app.get('/node_get_dances_by_shows', function (req, res) {
+    // TODO: Maybe find??
+});
+
+//Add new or Update Show to the database
+app.post("/node_add_show", (req, res) => {
+
+    const show = req.body.show;
+
+    if (show._id) {
+        // Update existed show
+        Show.updateOne({ _id: show._id },
+            { $set: show },
+            { runValidators: true },
+            (err, info) => {
+                if (err) {
+                    res.send({
+                        "message": err,
+                        "show": show,
+                    });
+                } else {
+                    res.send({
+                        "message": "success"
+                    })
+                }
             });
-        } else {
-            res.send({
-                "message": "success",
-                "data": data
-            })
+    } else {
+        // create a new movie
+        const newShow = new Show(show);
+        newShow.save(
+            (err, new_show) => {
+                if (err) {
+                    res.send({
+                        "message": err,
+                        "show": show
+                    });
+                } else {
+                    res.send({
+                        "message": "success",
+                    });
+                }
+            });
+    }
+});
+
+//Add new or Update Dance to the database
+//If New push it's Id onto Show Dance List
+app.post("/node_add_dance", (req, res) => {
+    // TODO: Figure out how to only add it to specified show
+
+    const dance = req.body.dance;
+    const show_id = req.body.show_id;
+    const order = req.body.order;
+
+    if (dance._id) {
+        // Update existed dance
+        Dance.updateOne({ _id: dance._id },
+            { $set: dance },
+            { runValidators: true },
+            (err, info) => {
+                if (err) {
+                    res.send({
+                        "message": err,
+                        "dance": dance,
+                    });
+                } else {
+                    res.send({
+                        "message": "success"
+                    })
+                }
+            });
+    } else {
+        // create a new movie
+        const newDance = new Dance(dance);
+        newDance.save(
+            (err, new_dance) => {
+                if (err) {
+                    res.send({
+                        "message": err,
+                        "dance": dance
+                    });
+                } else {
+                    // Add New Dance Id to show's dance list
+                    const dance_for_list = {
+                        dance_id: new_dance._id,
+                        sort_order: order + 1
+                    };
+
+                    Show.updateOne({ _id: show_id },
+                        {
+                            $push: { dances: dance_for_list }
+                        },
+                        {},
+                        (err, info) => {
+                            if (err) {
+                                res.send({
+                                    message: "database error"
+                                });
+                            } else {
+                                res.send({
+                                    message: "success"
+                                });
+                            }
+                        }
+                    );
+                }
+            }
+        );
+    }
+});
+
+
+// Add new Cue
+app.post('/node_add_cue', (req, res) => {
+    //Add the cue to the dance
+    const dance_id = req.body.dance_id;
+    const cue = req.body.cue;
+
+    Dance.updateOne(
+        { _id: dance_id},
+        {
+            $push: { lights: cue }
+        },
+        {},
+        (err, info) => {
+            if (err) {
+                res.send({
+                    message: "database error"
+                });
+            } else {
+                res.send({
+                    message: "success"
+                });
+            }
         }
-    })
+    );
 });
