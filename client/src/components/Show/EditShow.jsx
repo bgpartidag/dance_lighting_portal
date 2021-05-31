0ec11b09ac0b2cb4d965165e0d2d309344d9acee
bpartida <bpartida@clarku.edu>
2021-05-30 20:42:43 -0400

    Brenda: more schema rework

--- client/src/components/Show/EditShow.jsx
+++ client/src/components/Show/EditShow.jsx
@@ -11,15 +11,20 @@ function EditShow() {
 		setError('A test run');
         const form = event.target.elements;
 		console.log("The Start Date: " + form.show_start.value);
-		console.log("The Start Time: " + form.show_start_time.value);
 		const show = {
 			team_username : "TBD",
 			show_name : form.show_name.value,
 			contact_name : form.contact_name.value,
 			contact_email : form.contact_email.value,
 			contact_phone : form.contact_phone.value,
-			show_dates : ["00-00-0000", "11-11-1111"],
-			tech_dates : [form.show_start_time.value, "44-44-4444"],
+			show_start_date : "00-00-0000",
+			show_end_date: "11-11-1111",
+			show_start_time: form.show_start_time.value,
+			show_end_time: form.show_end_time.value,
+			tech_start_date : "33-33-3333", 
+			tech_end_date: "44-44-4444",
+			tech_start_time: form.tech_start_time.value,
+			tech_end_time: form.tech_end_time.value,
 			show_notes : form.show_detail.value
 		}
 		console.log(show);
