f6baffa1258ed6097a375af811355a5303be26eb
sarahVacca <sarahvacca3@gmail.com>
2021-05-31 20:02:51 -0400

    Header Completed!

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import $ from "jquery";
 import { Link } from "react-router-dom";
 
+
+// User Log in function
 function UserLogin(props) {
 	const currentUser = null;
 	const setCurrentUser = null;
@@ -27,14 +29,14 @@ function UserLogin(props) {
 		);
 	} else {
 		return (
-			<div className="navbar-nav navbar-right">
-				<Link className="nav-link active" to="/login">
+			<ul class="navbar-nav mb-2 mb-lg-0 navbar-right">
+				<Link className="nav-link active justify-content-end" to="/login">
 					Login
 				</Link>
 				<Link className="nav-link active" to="/register">
 					Register
 				</Link>
-			</div>
+			</ul>
 		);
 	}
 }
@@ -42,22 +44,28 @@ function UserLogin(props) {
 function Header(props) {
 	return (
 		<section id="header_section">
-			<nav className="navbar navbar-expand-lg nav-custom">
-				<div className="container-fluid">
-					<div className="navbar-nav">
-						<Link className="navbar-brand" to="/">
-							<img
-								src="/images/clark.png"
-								alt=""
-								width="50"
-								height="50"
-								class="d-inline-block align-text-top"
-							/>
-							CU Dance and Lighting Portal
-						</Link>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="container-fluid">
+					{/* Adds image and text to header */}
+					{/* The href sets up the img and text as a link to the home page */}
+					<a class="navbar-brand" href="/"> 
+						<img
+							src="/images/clark.png"
+							alt=""
+							width="50"
+							height="50"
+							class="d-inline-block align-text-middle" // changed class to middle to center text next to image
+							style={{ marginRight: "10px" }} // Adds space between img and text
+						/>
+					CU Dance and Lighting Portal</a>
+					{/* Sets up an appearing drop down menu when the browser is in a smaller sized window */}
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+							{/* Calls userlogin which adds the user login and the register buttons to the appearing drop down menu */}
+							<UserLogin {...props} />
 					</div>
-					{/*... extends dictionary with props dict*/}
-					<UserLogin {...props} />
 				</div>
 			</nav>
 		</section>
