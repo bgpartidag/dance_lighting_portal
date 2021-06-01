01366b41f664dda54cdcc92a25e8e2f6ae210b8a
Joshua Ofosu-Amoani <j.ofosuamoani@yahoo.com>
2021-06-01 01:12:15 -0400

    syled the home page

--- client/src/components/Home/ShowItem.jsx
+++ client/src/components/Home/ShowItem.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import $ from "jquery";
 
 function ShowItem(props) {
-	const row_class = "row " + props.evenOdd;
+	const row_class = "row";
 	const show = props.show;
 
 	return (
 		<div className={row_class}>
-			<div className="col-md-2">
+			<div className="col-md-2" style={{ borderRight: "1px solid black" }}>
 				<Link
-					className="show_name btn-primary"
+					className="show_name btn btn-primary"
 					to={{
 						pathname: "/edit_show",
 						state: {
@@ -18,23 +18,38 @@ function ShowItem(props) {
 							show_id: "A show id i guess",
 						},
 					}}
+					style={{ padding: "5px", margin: "1px" }}
 				>
-					<h5 style={{ color: "blue", textAlign: "left" }}>{show.show_name}</h5>
+					<h5 style={{ color: "white", textAlign: "center" }}>
+						{show.show_name}
+					</h5>
 				</Link>
 			</div>
-			<div className="col-md-2">
+			<div
+				className="col-md-2"
+				style={{ borderRight: "1px solid black", paddingTop: "8px" }}
+			>
 				<p>{show.team_name}</p>
 			</div>
-			<div className="col-md-2">
+			<div
+				className="col-md-2"
+				style={{ borderRight: "1px solid black", paddingTop: "8px" }}
+			>
 				<p>{show.start_date}</p>
 			</div>
-			<div className="col-md-2">
+			<div
+				className="col-md-2"
+				style={{ borderRight: "1px solid black", paddingTop: "8px" }}
+			>
 				<p>{show.end_date}</p>
 			</div>
-			<div className="col-md-2">
+			<div
+				className="col-md-2"
+				style={{ borderRight: "1px solid black", paddingTop: "8px" }}
+			>
 				<p>{show.start_time}</p>
 			</div>
-			<div className="col-md-2">
+			<div className="col-md-2" style={{ paddingTop: "8px" }}>
 				<p>{show.end_time}</p>
 			</div>
 		</div>
